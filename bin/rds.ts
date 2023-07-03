1bb8f6c3e91428956bec17a8371102ccdc4c0d79
ishan <ishan.na@soprasteria.com>
2023-07-03 23:44:25 +0530

    sta not found in bin fire at line 12 although we dont have any such case in the code

--- bin/rds.ts
+++ bin/rds.ts
@@ -2,34 +2,23 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SG } from '../lib/sg';
-import { MyStack } from '../lib/sg-part2';
-import { SGStack } from '../lib/sg-part3';
-import { App } from '@aws-cdk/core';
-import * as fs from 'fs';
 
 const app = new cdk.App();
-
-
-new SG(app, 'SGStack', {
-  
-   
-  env: { account: '656339507229', region: 'us-east-1' },
-
- 
+const stack = new cdk.Stack;
+// Read the command line arguments
+const ipAddressesParam = new cdk.CfnParameter(stack, 'IpAddressesParam', {
+  type: 'String',
+});
+const portsParam = new cdk.CfnParameter(sta, 'PortsParam', {
+  type: 'String',
 });
 
-// Read the cdk.json file and retrieve the IP addresses
+const ipAddresses = ipAddressesParam.valueAsString.split(',');
+const ports = portsParam.valueAsString.split(',');
 
+new SG(app, 'SGStack', {
+  ipAddresses,
+  ports,
+  env: { account: '656339507229', region: 'us-east-1' },
+});
 
-
-
-// Define the stack and pass the IP addresses as parameters
-// new MyStack(app, 'MyStack', {
-//   env: { account: '656339507229', region: 'us-east-1'},
-//   ipAddress1,
-//   ipAddress2,
-// });
-// new SGStack(app, 'MyStack', {
-//   env: { account: '656339507229', region: 'us-east-1'}
-  
-// });
--- lib/sg.ts
+++ lib/sg.ts
@@ -1,74 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { CfnParameter } from 'aws-cdk-lib';
-import { SecurityGroup, Peer, Port } from 'aws-cdk-lib/aws-ec2';
-import { Fn } from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
+interface SGProps extends cdk.StackProps {
+  ipAddresses: string[];
+  port: number;
+}
 
+export class SG extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props: SGProps) {
+    super(scope, id, props);
 
-export class SG extends cdk.Stack{
+    const { ipAddresses } = props;
 
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
+    // VPC creation
+    const vpc = new ec2.Vpc(this, "my-cdk-vpc", {
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: "public-subnet-1",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+      ],
+    });
 
-      //vpc creation
-      const vpc = new ec2.Vpc(this, "my-cdk-vpc", {
-        cidr: "10.0.0.0/16",
-        natGateways: 0,
-        maxAzs: 3,
-        subnetConfiguration: [
-          {
-            name: "public-subnet-1",
-            subnetType: ec2.SubnetType.PUBLIC,
-            cidrMask: 24,
-          },
-        ],
-      });
+    // Create the security group
+    const securityGroup = new ec2.SecurityGroup(this, "MySecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
+    });
 
-      const ipAddressParam = new cdk.CfnParameter(this, "IpAddressParam", {
-        type: "List<String>",
-      });
-      // Access the parameter value in your CDK stack code
-      const ipAddresses = ipAddressParam.valueAsList;
-
-      const portsParam = new CfnParameter(this, "Ports", {
-        type: "List<String>",
-        description: "Enter the ports (comma-separated)",
-        default: this.node.tryGetContext("Ports") || "443,8080,22", // Default ports
-      });
-
-      const ports = portsParam.valueAsList.map((port: string) => port);
-      // Create the security group
-      const securityGroup = new SecurityGroup(this, "MySecurityGroup", {
-        vpc: vpc, // Replace with your VPC reference
-        allowAllOutbound: true,
-      });
-
-      // Add inbound rules using the parsed IP addresses and ports
-      let portnumber: Port;
-      // for (const ipAddress of ipAddresses) {
-      //   for (const port of ports) {
-      //     securityGroup.addIngressRule(
-      //       Peer.ipv4(ipAddress),
-      //       type Port  number;
-      //       portnumber = parseInt(port, 10) as unknown;
-      //       portnumber = portnumber as Port;
-      //       Port.tcp(portnumber),
-      //       `Allow inbound traffic from ${ipAddress}:${port}`
-      //     );
-      //   }
-      // }
-      for (const ipAddress of ipAddresses) {
-        for (const port of ports) {
-          portnumber = parseInt(port, 10) as unknown as Port;
-          securityGroup.addIngressRule(
-            Peer.ipv4(ipAddress),
-            // Port.tcp(portnumber),
-            portnumber,
-            `Allow inbound traffic from ${ipAddress}:${port}`
-          );
-        }
+    // Add inbound rules using the provided IP addresses and ports
+    for (const ipAddress of ipAddresses) {
+      // for (const portStr of ports) {
+        // const portNumber = parseInt(portStr, 10);
+        const port = ec2.Port.tcp(80);
+        securityGroup.addIngressRule(
+          ec2.Peer.ipv4(ipAddress),
+          port,
+          `Allow inbound traffic from ${ipAddress}:80`
+        );
       }
     }
+  }
 
-}
